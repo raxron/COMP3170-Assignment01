232f9b499ad16f3a621ff93db540ed8d544ea5b6
RachelX <113003027+raxron@users.noreply.github.com>
2023-09-16 14:52:24 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Card from "./Card.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="app__title">World Business News</h1>
+      <Card />
+    </div>
+  );
+}
---
+++ src/Card.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+import JsonData from "./news.json";
+
+export default function Card() {
+  function limitDescription(description, maxLength) {
+    if (description && description.length > 0) {
+      const words = description.split(" ");
+      if (words.length > maxLength) {
+        return words.slice(0, maxLength).join(" ") + "...";
+      }
+      return description;
+    }
+    return "";
+  }
+
+  const displayData = JsonData.results.slice(0, 4).map((info) => {
+    function imgReplace() {
+      if (info.image_url === null) {
+        return "https://fastly.picsum.photos/id/439/536/354.jpg?hmac=cr_mJl3_TiOZLEY3Q90ui03r-EKYwrz-Ij5z3WPhSHo";
+      } else {
+        return info.image_url;
+      }
+    }
+
+    const limitDesc = limitDescription(info.description, 25);
+
+    return (
+      <>
+        <div className="story" key={info.id}>
+          <div className="story__img">
+            <img src={imgReplace()} alt="" />
+          </div>
+
+          <div className="story__box">
+            <h1 className="story__header">
+              <a href={info.link}>{info.title}</a>
+            </h1>
+            <p className="story__desc">{limitDesc}</p>
+            <a href={info.link} className="story__link">
+              Read More
+            </a>
+          </div>
+        </div>
+      </>
+    );
+  });
+
+  return <div className="container">{displayData}</div>;
+}
